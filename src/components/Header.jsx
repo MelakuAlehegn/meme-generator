0485255d75b7c8f36d260ba06acee4b3ff0a7434
melakualehegn <melakualehegn34@gmail.com>
2022-12-11 22:19:00 +0300

    logo image fix

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -4,7 +4,7 @@ function Header() {
   return (
     <header>
       <div className='logo'>
-        <img src='src/assets/Troll Face.png'/>
+        <img src='public/troll.png'/>
         <h3>Meme Generator</h3>
       </div>
       <h4 className='right-header'>React Course - Project 3</h4>
